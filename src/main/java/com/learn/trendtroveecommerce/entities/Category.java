1bd9291cfb3be00dd3672ca6a1275bd45eed31a5
Pankaj Bind <pankajbind30@gmail.com>
2023-10-22 12:36:11 +0530

    acategory and product class updated

--- src/main/java/com/learn/trendtroveecommerce/entities/Category.java
+++ src/main/java/com/learn/trendtroveecommerce/entities/Category.java
@@ -2,9 +2,12 @@
 package com.learn.trendtroveecommerce.entities;
 
 import jakarta.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Category {
@@ -13,4 +16,62 @@ public class Category {
     private int categoryId;
     private String categoryTitle;
     private String categoryDescription;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products = new ArrayList<>();
+     
+    public Category() {
+    }
+
+    public Category(int categoryId, String categoryTitle, String categoryDescription) {
+        this.categoryId = categoryId;
+        this.categoryTitle = categoryTitle;
+        this.categoryDescription = categoryDescription;
+    }
+
+    public Category(String categoryTitle, String categoryDescription, List<Product> products) {
+        this.categoryTitle = categoryTitle;
+        this.categoryDescription = categoryDescription;
+        this.products = products;
+    }
+    
+    
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryTitle() {
+        return categoryTitle;
+    }
+
+    public void setCategoryTitle(String categoryTitle) {
+        this.categoryTitle = categoryTitle;
+    }
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Category{" + "categoryId=" + categoryId + ", categoryTitle=" + categoryTitle + ", categoryDescription=" + categoryDescription + '}';
+    }
+    
+    
 }
--- src/main/java/com/learn/trendtroveecommerce/entities/Product.java
+++ src/main/java/com/learn/trendtroveecommerce/entities/Product.java
@@ -1,13 +1,110 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.learn.trendtroveecommerce.entities;
 
-/**
- *
- * @author Pankaj Bind
- */
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pId;
+    private String pName;
+    @Column(length = 3000)
+    private String pDesc;
+    private String pPhoto;
+    private int pPrice;
+    private int pDiscount;
+    private int pQuantity;
+    @ManyToOne
+    private Category category;
+    public Product() {
+    }
+
+    public Product(int pId, String pName, String pDesc, String pPhoto, int pPrice, int pDiscount, int pQuantity, Category category) {
+        this.pId = pId;
+        this.pName = pName;
+        this.pDesc = pDesc;
+        this.pPhoto = pPhoto;
+        this.pPrice = pPrice;
+        this.pDiscount = pDiscount;
+        this.pQuantity = pQuantity;
+        this.category = category;
+    }
+
+    public int getpId() {
+        return pId;
+    }
+
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpDesc() {
+        return pDesc;
+    }
+
+    public void setpDesc(String pDesc) {
+        this.pDesc = pDesc;
+    }
+
+    public String getpPhoto() {
+        return pPhoto;
+    }
+
+    public void setpPhoto(String pPhoto) {
+        this.pPhoto = pPhoto;
+    }
+
+    public int getpPrice() {
+        return pPrice;
+    }
+
+    public void setpPrice(int pPrice) {
+        this.pPrice = pPrice;
+    }
+
+    public int getpDiscount() {
+        return pDiscount;
+    }
+
+    public void setpDiscount(int pDiscount) {
+        this.pDiscount = pDiscount;
+    }
+
+    public int getpQuantity() {
+        return pQuantity;
+    }
+
+    public void setpQuantity(int pQuantity) {
+        this.pQuantity = pQuantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
+    @Override
+    public String toString() {
+        return "Product{" + "pId=" + pId + ", pName=" + pName + ", pDesc=" + pDesc + ", pPhoto=" + pPhoto + ", pPrice=" + pPrice + ", pDiscount=" + pDiscount + ", pQuantity=" + pQuantity + '}';
+    }
+
     
 }
