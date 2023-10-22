a3239ca43a70181f9244a010ff2932c099735520
Pankaj Bind <pankajbind30@gmail.com>
2023-10-22 12:13:18 +0530

    category entity updated

--- src/main/java/com/learn/trendtroveecommerce/entities/Category.java
+++ src/main/java/com/learn/trendtroveecommerce/entities/Category.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.learn.trendtroveecommerce.entities;
 
-/**
- *
- * @author Pankaj Bind
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int categoryId;
+    private String categoryTitle;
+    private String categoryDescription;
 }
