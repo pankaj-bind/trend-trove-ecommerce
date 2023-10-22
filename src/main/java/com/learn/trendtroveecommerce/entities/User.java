1d6845049f1417b45e6281e67f84ff5f10d72658
Pankaj Bind <pankajbind30@gmail.com>
2023-10-22 11:58:08 +0530

    user and category class created

---
+++ src/main/java/com/learn/trendtroveecommerce/entities/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.learn.trendtroveecommerce.entities;
+
+/**
+ *
+ * @author Pankaj Bind
+ */
+public class User {
+    
+}
