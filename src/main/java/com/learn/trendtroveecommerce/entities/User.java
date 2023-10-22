036d4df2de625e4faf56e11fc912ae2274f79b23
Pankaj Bind <pankajbind30@gmail.com>
2023-10-22 12:09:27 +0530

    user entity updated

--- src/main/java/com/learn/trendtroveecommerce/entities/User.java
+++ src/main/java/com/learn/trendtroveecommerce/entities/User.java
@@ -1,13 +1,113 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.learn.trendtroveecommerce.entities;
 
-/**
- *
- * @author Pankaj Bind
- */
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 10, name = "user_id")
+    private int userId;
+    @Column(length = 100, name = "user_name")
+    private String userName;
+    @Column(length = 100, name = "user_email")
+    private String userEmail;
+    @Column(length = 100, name = "user_password")
+    private String userPassword;
+    @Column(length = 12, name = "user_phone")
+    private String userPhone;
+    @Column(length = 1500, name = "user_pic")
+    private String userPic;
+    @Column(length = 1500, name = "user_address")
+    private String userAddress;
+
+    public User(int userId, String userName, String userEmail, String userPassword, String userPhone, String userPic, String userAddress) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userPhone = userPhone;
+        this.userPic = userPic;
+        this.userAddress = userAddress;
+    }
+
+    public User(String userName, String userEmail, String userPassword, String userPhone, String userPic, String userAddress) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userPhone = userPhone;
+        this.userPic = userPic;
+        this.userAddress = userAddress;
+    }
+
+    public User() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserPic() {
+        return userPic;
+    }
+
+    public void setUserPic(String userPic) {
+        this.userPic = userPic;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail + ", userPassword=" + userPassword + ", userPhone=" + userPhone + ", userPic=" + userPic + ", userAddress=" + userAddress + '}';
+    }
+    
     
 }
